955e86e74657a79b95c9ae2ede75bb05e26ade29
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-15 00:26:01 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,7 @@
 module Findable
+  module ClassMethods 
   def self.find_by_name(name)
-    self.class.all.detect{|a| a.name == name}
+    self.all.detect{|a| a.name} 
+    #== name}
   end 
-end 
+end
